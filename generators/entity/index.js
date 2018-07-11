2bced3a845aae182a439a19e51e256f3ba15ef5f
Victor Chow <285050252@qq.com>
2018-07-11 17:37:16 +0800

    add the option to delete generatorConfig.xml

--- generators/entity/index.js
+++ generators/entity/index.js
@@ -1,14 +1,16 @@
-'use strict';
-const Generator = require('yeoman-generator');
+"use strict";
+const Generator = require("yeoman-generator");
 // import { Generator } from 'yeoman-generator';
-const shell  = require('shelljs');
-const prompts = require('./prompts');
+const shell = require("shelljs");
+const prompts = require("./prompts");
+const fileSys = require("fs");
 
 module.exports = class extends Generator {
+
   constructor(args, opts) {
     super(args, opts);
 
-    console.log('preparing generator');
+    console.log("preparing generator");
 
     this.DBHost = this.options.DBHost;
     this.DBPort = this.options.DBPort;
@@ -26,7 +28,7 @@ module.exports = class extends Generator {
 
   get prompting() {
     // Have Yeoman greet the user.
-    console.log('prompting configuaration');
+    console.log("prompting configuaration");
     return {
       askForDBHost: prompts.askForDBHost,
       askForDBPort: prompts.askForDBPort,
@@ -42,126 +44,162 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    shell.echo('=================================================');
+    const self = this;
 
-    if (shell.which('java')) {
-      shell.echo('Find java command');
-      shell.echo('=================================================');
+    // 由于没找到覆盖参数，临时判断删除
+    if (fileSys.existsSync(this.destinationPath("generatorConfig.xml"))) {
+      const done = this.async();
+      const prompts = [
+        {
+          type: "input",
+          name: "delConfig",
+          message: "Note: generatorConfig.xml already exists. Delete it? :",
+          default: "yes"
+        }
+      ];
+      this.prompt(prompts).then(props => {
+        const v = props.delConfig.toLowerCase();
+        if (v === "yes" || v === "y") {
+          done();
+          fileSys.unlinkSync(this.destinationPath("generatorConfig.xml"));
+          self._processing(self);
+        }
+      });
     } else {
-      shell.echo('Sorry, this script requires java framework');
-      shell.echo('=================================================');
+      self._processing(self);
+    }
+  }
+
+  install() {
+    // console.log("install");
+    // this.installDependencies({
+    //   bower: false,
+    //   yarn: false,
+    //   npm: false
+    // });
+  }
+
+  end() {
+    // console.log("end");
+  }
+
+  _processing(self) {
+    // shell.echo("=================================================");
+    if (shell.which("java")) {
+      // shell.echo("Find java command");
+      // shell.echo("=================================================");
+    } else {
+      shell.echo("Sorry, this script requires java framework");
+      // shell.echo("=================================================");
       shell.exit(1);
     }
 
-    var templateOptions = {};
-    templateOptions.DBHost = this.DBHost;
-    templateOptions.DBPort = this.DBPort;
-    templateOptions.DBName = this.DBName;
-    templateOptions.DBUserName = this.DBUserName;
-    templateOptions.DBPassword = this.DBPassword;
-    templateOptions.entityTableName = this.entityTableName;
 
-    templateOptions.domainName = this.domainName;
-    templateOptions.packageName = this.packageName;
-    templateOptions.subPackageName = this.subPackageName;
+    let templateOptions = {};
+    templateOptions.DBHost = self.DBHost;
+    templateOptions.DBPort = self.DBPort;
+    templateOptions.DBName = self.DBName;
+    templateOptions.DBUserName = self.DBUserName;
+    templateOptions.DBPassword = self.DBPassword;
+    templateOptions.entityTableName = self.entityTableName;
+
+    templateOptions.domainName = self.domainName;
+    templateOptions.packageName = self.packageName;
+    templateOptions.subPackageName = self.subPackageName;
 
     // 新增一个变量-domain的驼峰样式和小写样式
     templateOptions.lowerDomainName =
-      this.domainName.substring(0, 1).toLowerCase() +
-      this.domainName.substring(1);
-    templateOptions.allLowerDomainName = this.domainName.toLowerCase();
+      self.domainName.substring(0, 1).toLowerCase() +
+      self.domainName.substring(1);
+    templateOptions.allLowerDomainName = self.domainName.toLowerCase();
 
-    // 由于没找到覆盖参数，临时判断删除
-    if (this.fs.exists(this.destinationPath('generatorConfig.xml'))) {
-      this.fs.delete(this.destinationPath('generatorConfig.xml'));
-    }
 
-    this.fs.copyTpl(
-      this.templatePath('generatorConfig.xml'),
-      this.destinationPath('generatorConfig.xml'),
+    self.fs.copyTpl(
+      self.templatePath("generatorConfig.xml"),
+      self.destinationPath("generatorConfig.xml"),
       templateOptions
     );
 
-    var cpargs = [
-      this.templatePath() + '/copyfiles.sh',
-      this.templatePath('lib'),
-      this.destinationPath('lib')
+    let cpargs = [
+      self.templatePath() + "/copyfiles.sh",
+      self.templatePath("lib"),
+      self.destinationPath("lib")
     ];
 
-    this.spawnCommandSync('bash', cpargs);
+    self.spawnCommandSync("bash", cpargs);
 
-    if (!this.fs.exists(this.destinationPath('src/main/java'))) {
-      this.spawnCommandSync('mkdir', [
-        '-p',
-        this.destinationPath('src/main/java')
+    if (!fileSys.existsSync(self.destinationPath("src/main/java"))) {
+      self.spawnCommandSync("mkdir", [
+        "-p",
+        self.destinationPath("src/main/java")
       ]);
     }
 
-    if (!this.fs.exists(this.destinationPath('src/main/resources/mapper'))) {
-      this.spawnCommandSync('mkdir', [
-        '-p',
-        this.destinationPath('src/main/resources/mapper')
+    if (!fileSys.existsSync(self.destinationPath("src/main/resources/mapper"))) {
+      self.spawnCommandSync("mkdir", [
+        "-p",
+        self.destinationPath("src/main/resources/mapper")
       ]);
     }
 
-    var self = this;
+
     setTimeout(function() {
       if (
         shell.exec(
-          'java -Djava.ext.dirs=./lib -jar ./lib/mybatis-generator-core-1.3.6.jar -configfile generatorConfig.xml -overwrite'
+          "java -Djava.ext.dirs=./lib -jar ./lib/mybatis-generator-core-1.3.6.jar -configfile generatorConfig.xml -overwrite"
         ).code !== 0
       ) {
-        shell.echo('Error: mybatis generate failed');
+        shell.echo("Error: mybatis generate failed");
         shell.exit(1);
       } else {
-        var localPackageName = self.packageName;
-        var packageNameTemp = localPackageName.replace('.', '/');
-        var domainDir = self.destinationPath(
-          'src/main/java/' +
-            packageNameTemp +
-            '/domain/' +
-            self.subPackageName +
-            '/'
+        const localPackageName = self.packageName;
+        const packageNameTemp = localPackageName.replace(".", "/");
+        const domainDir = self.destinationPath(
+          "src/main/java/" +
+          packageNameTemp +
+          "/domain/" +
+          self.subPackageName +
+          "/"
         );
-        var dtoDir = self.destinationPath(
-          'src/main/java/' +
-            packageNameTemp +
-            '/domain/' +
-            self.subPackageName +
-            '/dto/'
+        const dtoDir = self.destinationPath(
+          "src/main/java/" +
+          packageNameTemp +
+          "/domain/" +
+          self.subPackageName +
+          "/dto/"
         );
 
-        if (!self.fs.exists(dtoDir)) {
-          self.spawnCommandSync('mkdir', ['-p', dtoDir]);
+        if (!fileSys.existsSync(dtoDir)) {
+          self.spawnCommandSync("mkdir", ["-p", dtoDir]);
         }
 
         // 创建dto文件
-        var domainFileStr = self.fs.read(domainDir + self.domainName + '.java');
-        var dtoFileStr = domainFileStr.replace(
-          'class ' + self.domainName,
-          'class ' + self.domainName + 'Dto'
+        const domainFileStr = self.fs.read(domainDir + self.domainName + ".java");
+        const dtoFileStr = domainFileStr.replace(
+          "class " + self.domainName,
+          "class " + self.domainName + "Dto"
         ).replace(
-          self.packageName + '.domain.' + self.subPackageName,
-          self.packageName + '.domain.' + self.subPackageName + '.dto'
+          self.packageName + ".domain." + self.subPackageName,
+          self.packageName + ".domain." + self.subPackageName + ".dto"
         );
-        self.fs.write(dtoDir + self.domainName + 'Dto.java', dtoFileStr);
+        self.fs.write(dtoDir + self.domainName + "Dto.java", dtoFileStr);
 
         // 创建mapstruct文件
-        var mapStructSourceFile = self.templatePath(
-          'src/com/base/controller/mapstruct/_EntityMapStruct.java'
+        const mapStructSourceFile = self.templatePath(
+          "src/com/base/controller/mapstruct/_EntityMapStruct.java"
         );
-        var mapStructTargetDir = self.destinationPath(
-          'src/main/java/' +
-            packageNameTemp +
-            '/controller/mapstruct/' +
-            self.subPackageName +
-            '/'
+        const mapStructTargetDir = self.destinationPath(
+          "src/main/java/" +
+          packageNameTemp +
+          "/controller/mapstruct/" +
+          self.subPackageName +
+          "/"
         );
-        var mapStructTargetFile =
-          mapStructTargetDir + self.domainName + 'MapStruct.java';
+        const mapStructTargetFile =
+          mapStructTargetDir + self.domainName + "MapStruct.java";
 
-        if (!self.fs.exists(mapStructTargetDir)) {
-          self.spawnCommandSync('mkdir', ['-p', mapStructTargetDir]);
+        if (!fileSys.existsSync(mapStructTargetDir)) {
+          self.spawnCommandSync("mkdir", ["-p", mapStructTargetDir]);
         }
 
         self.fs.copyTpl(
@@ -171,57 +209,62 @@ module.exports = class extends Generator {
         );
 
         // 创建Service和controller的路径，拷贝Service和controller
-        var serviceSourceFilePath = self.templatePath(
-          'src/com/base/service/_EntityService.java'
+        const serviceSourceFilePath = self.templatePath(
+          "src/com/base/service/_EntityService.java"
         );
-        var serviceTargetDir = self.destinationPath(
-          'src/main/java/' +
-            packageNameTemp +
-            '/service/' +
-            self.subPackageName +
-            '/'
+        const serviceTargetDir = self.destinationPath(
+          "src/main/java/" +
+          packageNameTemp +
+          "/service/" +
+          self.subPackageName +
+          "/"
         );
-        if (!self.fs.exists(serviceTargetDir)) {
-          self.spawnCommandSync('mkdir', ['-p', serviceTargetDir]);
+        if (!fileSys.existsSync(serviceTargetDir)) {
+          self.spawnCommandSync("mkdir", ["-p", serviceTargetDir]);
         }
         self.fs.copyTpl(
           serviceSourceFilePath,
-          serviceTargetDir + self.domainName + 'Service.java',
+          serviceTargetDir + self.domainName + "Service.java",
           templateOptions
         );
 
-        var controllerSourceFilePath = self.templatePath(
-          'src/com/base/controller/_EntityController.java'
+        const controllerSourceFilePath = self.templatePath(
+          "src/com/base/controller/_EntityController.java"
         );
-        var controllerTargetDir = self.destinationPath(
-          'src/main/java/' +
-            packageNameTemp +
-            '/controller/' +
-            self.subPackageName +
-            '/'
+        const controllerTargetDir = self.destinationPath(
+          "src/main/java/" +
+          packageNameTemp +
+          "/controller/" +
+          self.subPackageName +
+          "/"
         );
-        if (!self.fs.exists(controllerTargetDir)) {
-          self.spawnCommandSync('mkdir', ['-p', controllerTargetDir]);
+        if (!fileSys.existsSync(controllerTargetDir)) {
+          self.spawnCommandSync("mkdir", ["-p", controllerTargetDir]);
         }
         self.fs.copyTpl(
           controllerSourceFilePath,
-          controllerTargetDir + self.domainName + 'Controller.java',
+          controllerTargetDir + self.domainName + "Controller.java",
           templateOptions
         );
       }
-    }, 3000);
+
+      self._deleteFolder(self.destinationPath("lib/"))
+    }, 2000);
   }
 
-  install() {
-    console.log('install');
-    // this.installDependencies({
-    //   bower: false,
-    //   yarn: false,
-    //   npm: false
-    // });
+  _deleteFolder(path) {
+    let files = [];
+    if (fileSys.existsSync(path)) {
+      files = fileSys.readdirSync(path);
+      files.forEach(function(file, index) {
+        const curPath = path + "/" + file;
+        if (fileSys.statSync(curPath).isDirectory()) { // recurse
+          deleteFolder(curPath);
+        } else {
+          fileSys.unlinkSync(curPath);
+        }
+      });
+      fileSys.rmdirSync(path);
+    }
   }
-
-  end() {
-    console.log('end');
-  }
-}
+};
